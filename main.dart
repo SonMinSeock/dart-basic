4244ee345c6bd675df29e970234a8cbb1eaef569
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-05-31 20:30:59 +0900

    feat: String Interpolation

--- main.dart
+++ main.dart
@@ -1,7 +1,8 @@
 void main() {
   // 리스트 선언 하는방법 두 가지 있음.
   // var numbers = [1, 2, 3, 4];
-  var giveMeFive = true;
-  List<int> numers = [1, 2, 3, 4, if (giveMeFive) 5];
-  print(numers);
+  var name = 'Son';
+  var age = 24;
+  var greeting = "Hello everyone, my name is $name, I'm ${age + 2} old.";
+  print(greeting);
 }
