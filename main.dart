8c87156979741bccaf193330f9d2f9b82424d3ba
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-05-31 21:08:34 +0900

    feat: Sets

--- main.dart
+++ main.dart
@@ -1,6 +1,16 @@
 void main() {
-  List<Map<String, Object>> players = [
-    {'name': 'Son', 'xp': 199932.323},
-    {'name': 'Nico', 'xp': 199932.634}
-  ];
+  // var numbers = {1, 2, 3, 4};
+  Set<int> numbers1 = {1, 2, 3, 4};
+  List<int> numbers2 = [1, 2, 3, 4];
+
+  numbers1.add(1);
+  numbers1.add(1);
+  numbers1.add(1);
+
+  numbers2.add(1);
+  numbers2.add(1);
+  numbers2.add(1);
+
+  print("Set 이용: $numbers1");
+  print("List 이용: $numbers2");
 }
