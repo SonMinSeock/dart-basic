bb6dba56707e9744d1fa6b8189201e4b344bdfee
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-05-31 19:57:16 +0900

    feat: Basic Data Types

--- main.dart
+++ main.dart
@@ -1,6 +1,8 @@
 void main() {
-  late final name;
-  // do something api...
-  name = 'Son';
-  print(name);
+  String name = 'Son';
+  bool alive = true;
+  int age = 26;
+  double money = 69.12;
+  num x = 12;
+  x = 15.56;
 }
