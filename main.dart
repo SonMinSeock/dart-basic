d177753f3fb02d49afa44becb89d2ca2ffb58882
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-05-31 20:13:07 +0900

    feat: Lists

--- main.dart
+++ main.dart
@@ -1,8 +1,7 @@
 void main() {
-  String name = 'Son';
-  bool alive = true;
-  int age = 26;
-  double money = 69.12;
-  num x = 12;
-  x = 15.56;
+  // 리스트 선언 하는방법 두 가지 있음.
+  // var numbers = [1, 2, 3, 4];
+  var giveMeFive = true;
+  List<int> numers = [1, 2, 3, 4, if (giveMeFive) 5];
+  print(numers);
 }
