814bc5632cf3a02c12cb32048bbe2373a280ded9
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-06-03 20:15:10 +0900

    feat: Defining a Function

--- main.dart
+++ main.dart
@@ -1,16 +1,8 @@
+String sayHello(String name) => "Hello $name nice to meet you!";
+
+num plus(a, b) => a + b;
+
 void main() {
-  // var numbers = {1, 2, 3, 4};
-  Set<int> numbers1 = {1, 2, 3, 4};
-  List<int> numbers2 = [1, 2, 3, 4];
-
-  numbers1.add(1);
-  numbers1.add(1);
-  numbers1.add(1);
-
-  numbers2.add(1);
-  numbers2.add(1);
-  numbers2.add(1);
-
-  print("Set 이용: $numbers1");
-  print("List 이용: $numbers2");
+  print(sayHello("Son"));
+  print(plus(10, 5));
 }
