6fbb4dcfc338d12cfdf08cecdb094177aab6f839
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-06-04 13:39:53 +0900

    feat: QQ Operator

--- main.dart
+++ main.dart
@@ -1,8 +1,6 @@
-String sayHello(String name, int age, [String? country = '대한민국']) {
-  return "Hello $name, you are $age, and you come from $country.";
-}
-
 void main() {
-  var results = sayHello("손민석", 27);
-  print(results);
+  String? name;
+
+  name ??= 'Son';
+  print(name);
 }
