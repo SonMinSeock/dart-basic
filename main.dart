6b3977c8435fdd3e715126aeeb7960db5d39d995
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-06-09 07:49:26 +0900

    feat: Named Constructors

--- main.dart
+++ main.dart
@@ -1,8 +1,7 @@
 class Player {
   final String name;
-  int xp;
+  int xp, age;
   String team;
-  int age;
 
   Player(
       {required this.name,
@@ -10,14 +9,26 @@ class Player {
       required this.team,
       required this.age});
 
+  Player.createBluePlayer({required String name, required int age})
+      : this.name = name,
+        this.age = age,
+        this.xp = 0,
+        this.team = 'blue';
+
+  Player.createRedPlayer(String name, int age)
+      : this.name = name,
+        this.age = age,
+        this.xp = 0,
+        this.team = 'red';
+
   void sayHello() {
     print("Hi my name is ${this.name}");
   }
 }
 
 void main() {
-  var player1 = Player(name: 'Son', xp: 1500, team: "blue", age: 26);
-  var player2 = Player(name: 'Lee', team: "red", xp: 1300, age: 24);
+  var player1 = Player.createBluePlayer(name: 'Son', age: 26);
+  var player2 = Player.createRedPlayer('Lee', 24);
 
   player1.sayHello();
   player2.sayHello();
