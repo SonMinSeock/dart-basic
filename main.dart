d8e88d84db792567e3245bafbb7daf4887cf727b
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2025-06-09 09:35:06 +0900

    feat: Mixins

--- main.dart
+++ main.dart
@@ -1,25 +1,21 @@
-class Human {
-  final String name;
-  Human({required this.name});
-  void sayHello() {
-    print("Hi my name is $name");
+mixin Strong {
+  final double strengthLevel = 1500.99;
+}
+
+mixin QuickRunner {
+  void runQuick() {
+    print("runnnnnn!");
   }
 }
 
 enum Team { red, blue }
 
-class Player extends Human {
+class Player with Strong, QuickRunner {
   final Team team;
-  Player({required this.team, required String name}) : super(name: name);
-
-  @override
-  void sayHello() {
-    super.sayHello();
-    print("and i play for ${team.name}");
-  }
+  Player({required this.team});
 }
 
 void main() {
-  var player1 = Player(team: Team.blue, name: 'Son');
-  player1.sayHello();
+  var player = Player(team: Team.blue);
+  player.runQuick();
 }
